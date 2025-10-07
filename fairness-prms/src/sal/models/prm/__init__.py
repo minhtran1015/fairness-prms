ce58186982aa87fc4b6372ad28db43102b5e22df
minhtran1015 <143114074+minhtran1015@users.noreply.github.com>
2025-10-07 11:59:43 +0700

    Remove unavailable PRMs from module exports
    Removed references to math_shepherd and rlhf_flow modules from __init__.py since they are not present in the repository and caused import errors. Only existing PRMs are now exposed.

--- fairness-prms/src/sal/models/prm/__init__.py
+++ fairness-prms/src/sal/models/prm/__init__.py
@@ -1,14 +1,18 @@
 from .base import PRM
-from .math_shepherd import MathShepherd
-from .rlhf_flow import RLHFFlow
-from .bias_detection import BiasDetectionPRM, LoraBiasDetectionPRM, OutcomeDetectionPRM, UntrainedBiasPRM
+# NOTE: math_shepherd and rlhf_flow modules are not present in this repository clone.
+# They were referenced originally but caused ModuleNotFoundError on Kaggle.
+# We expose only the PRMs that actually exist here.
+from .bias_detection import (
+    BiasDetectionPRM,
+    LoraBiasDetectionPRM,
+    OutcomeDetectionPRM,
+    UntrainedBiasPRM,
+)
 
 __all__ = [
     "PRM",
-    "MathShepherd",
-    "RLHFFlow",
     "BiasDetectionPRM",
     "LoraBiasDetectionPRM",
     "OutcomeDetectionPRM",
     "UntrainedBiasPRM",
-] 
+]
